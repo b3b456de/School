40758d118b31e1a3c51fc200a2cd04e2a52a8192
Samuel Spry <ss049913@ohio.edu>
2015-03-17 10:10:14 -0400

    Eval should work

--- hw3.rb
+++ hw3.rb
@@ -1,12 +1,8 @@
 #todo
-#  write the number identifier in the lexical analyzer
-#    Using regex
 #  write the parser
 #    Create tree
 #    Test Tree
-#  write the eval function
-#    easier if top down, and left most
-#    Probably should be recursive
+#  test eval function
 
 
 # .chr() transfer int value to character value  42.chr() == *
@@ -39,7 +35,33 @@ class Lex
       when ')'
         return "RPAREN", ')'
       when "0".."9"
-        number = check #/[1-9][0-9]*/.match(raw)
+        number = ""
+        #displacement variable
+        dis = 0
+        if /[1-9]/.match(check)
+          number << check
+          check = @tokens[@pos]
+          while /[0-9]/.match(check)
+            number << check
+            dis += 1
+            check = @tokens[@pos + dis]
+          end
+          if check == "."
+            number << check
+            dis += 1
+            check = @tokens[@pos+dis]
+          else
+            @pos += dis
+            return "NUMBER", number
+          end
+          while /[0-9]/.match(check)
+            number << check
+            dis+=1
+            check = @tokens[@pos+dis]
+          end
+          @pos += dis
+          return "NUMBER", number
+        end
         return "NUMBER", number
       when '/'
         return "DIV", '/'
@@ -61,10 +83,11 @@ end
 class Parse
   def gen_tree(lex)
     look_ahead = lex.next
-    root = Tree.new("prog")
+    @root = Tree.new("prog")
     while look_ahead != nil
       current = look_ahead
       look_ahead = lex.next
+      expr(current, look_ahead)
     end
   end
   def expr(token, look_ahead)
@@ -98,16 +121,20 @@ class Parse
     if token =="MUL"
     elsif token == "DIV"
     else
-      puts "Semantic Error"
+      puts "Syntactic Error"
       return nil
     end
   end
-  def number
+  def number(token, look_ahead)
     if token == "NUMBER"
     elsif
-      puts "Semantic Error"
+      puts "Syntactic Error"
       return nil
     end
+  end
+  def eval_tree
+    ev = Eval.new
+    Eval.eval_tree(@root)
   end
 end
 
@@ -125,6 +152,33 @@ class Tree
 end
 
 class Eval
+  def eval_tree(node)
+    if node.left == nil && node.right == nil
+      return eval_tree(node.center)
+    elsif node.left.data == "(" && node.right.data == ")"
+      return eval_tree(node.center)
+    elsif node.center.data = "add_op"
+      if node.center.center.data == "+"
+        return eval_tree(node.left) + eval_tree(node.right)
+      elsif node.center.center.data =="-"
+        return eval_tree(node.left) - eval_tree(node.right)
+      else
+        return "Error"
+      end
+    elsif node.center.data ="mul_op"
+      if node.center.center.data == "*"
+        return eval_tree(node.left) * eval_tree(node.right)
+      elsif node.center.center.data == "/"
+        return eval_tree(node.left) / eval_tree(node.right)
+      else
+        return "Error"
+      end
+    elsif node.center.data = "number"
+      return node.center.center.data.to_f
+    else
+      return "Error"
+    end
+  end
 end
 
 analyze = Lex.new
@@ -135,8 +189,9 @@ while(parse)
   print("Enter an expression to parse: ")
   input = gets.chomp
   analyze.st_lex(input)
-  for i in 1..3 do
+  for i in 1..5 do
     puts analyze.next
   end
   parser.gen_tree(analyze)
+  parser.eval_tree
 end
